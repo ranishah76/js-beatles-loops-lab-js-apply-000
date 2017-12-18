84b5def6c34b4a2c8e6bd4f3c8a23aef8fbf479c
Rani <rani76@gmail.com>
2017-12-18 13:02:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function theBeatlesPlay (musicians, instruments) {
 var array = []
 
-  for (i = 0; i < musicians.length; i++) {
-    text += "John Lennon plays the guitar";
+  for (i = 0; i < array.length; i++) {
+    return "${musicians} plays the ${instrumnets}";
 }
 }
