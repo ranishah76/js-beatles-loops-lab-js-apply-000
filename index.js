4a4b4d58d88d05e18d2508423046ee7cee7d26ad
Rani <rani76@gmail.com>
2017-12-18 13:06:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-function theBeatlesPlay (musicians, instruments) {
-const array = []
+function theBeatlesPlay(musicians, instruments) {
+  const arr = []
 
-  for (let i = 0; i < musicians.length; i < 1;  i++) {
-    array.push("${musicians[i]} plays ${instruments[i]}")
-}
+  for (let i = 0, l = musicians.length; i < l; i++) {
+    arr.push(`${musicians[i]} plays ${instruments[i]}`)
+  }
 
-return array
+  return arr
 }
